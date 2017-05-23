e7d909956e79e85dcd5a6ddeea2f2d5924dcc2ce
Link_Secret <zj1078184113@163.com>
2017-05-23 18:22:59 +0800

    commit TankWar1.7

---
+++ TankWar1.7/src/Missile.java
@@ -0,0 +1,100 @@
+import java.awt.*;
+
+public class Missile {
+	public static final int XSPEED = 10;
+	public static final int YSPEED = 10;
+
+	public static final int WIDTH = 10;
+	public static final int HIGHT = 10;
+	
+	
+
+	int x, y;
+	Tank.Direction dir;
+
+	private boolean live = true;
+	private TankClient tc;
+
+	public Missile(int x, int y, Tank.Direction dir) {
+		this.x = x;
+		this.y = y;
+		this.dir = dir;
+	}
+	
+	public Missile(int x,int y, Tank.Direction dir, TankClient tc){
+		this(x,y,dir);
+		this.tc =tc;
+	}
+
+	public void draw(Graphics g) {
+		if(!live){
+			tc.missiles.remove(this);
+			return;
+		}
+		
+		Color c = g.getColor();
+		g.setColor(Color.BLACK);
+		g.fillOval(x, y, WIDTH, HIGHT);
+		g.setColor(c);
+
+		move();
+	}
+
+	private void move() {
+		switch (dir) {
+		case L:
+			x = x - XSPEED;
+			break;
+		case LU:
+			x = x - XSPEED;
+			y = y - YSPEED;
+			break;
+		case U:
+			y = y - YSPEED;
+			break;
+		case RU:
+			x = x + XSPEED;
+			y = y - YSPEED;
+			break;
+		case R:
+			x = x + XSPEED;
+			break;
+		case RD:
+			x = x + XSPEED;
+			y = y + YSPEED;
+			break;
+		case D:
+			y = y + YSPEED;
+			break;
+		case LD:
+			x = x - XSPEED;
+			y = y + YSPEED;
+			break;
+		}
+
+		if (x < 0 || y < 0 || x > TankClient.GAME_WIDTH || y > TankClient.GAME_HIGHT) {
+			 live = false;
+//			 tc.missiles.remove(this);
+		}
+	}
+
+	public boolean isLive() {
+		return live;
+	}
+    
+	public Rectangle getRect(){
+		return new Rectangle(x,y,WIDTH,HIGHT);
+	}
+	
+	public boolean hitTank(Tank t){
+		
+		if(this.getRect().intersects(t.getRect()) && t.isLive()){
+			t.setLive(false);
+			this.live = false;
+			Explode e = new Explode(x, y, tc);
+			tc.Explodes.add(e);
+			return true;
+		}
+		return false;
+	}
+}
